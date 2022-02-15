bae903a258a860563d0c8274512de6729fa36593
Ian Ozsvald <ian@ianozsvald.com>
2022-02-15 15:46:13 +0000

    updates

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
+Uplift models determine if someone responds positively, neutrally or negatively to an intervention such as a marketing activity. Churn models are dominant but don't model a person's reaction to marketing, they're based on finding "who is likely to churn" not "who could be saved with marketing or another intervention".
+
+This code simulates a population who can respond positively or negatively to marketing or who have negative and positive associations with the company. A Churn model is built (ignoring marketing efforts), an Uplift model is built (where 50% get marketing and 50% don't with a random split in the population). The plotting code shows the outcome of these models.
+
+An Uplift model will "save" more people, it can identify who responds positively to marketing and identify who shouldn't be marketed at (as they'll respond neutrally - so there's no point, or negaively - so marketing hurts). 
+
+
+# Building the models
+
+* Run `make_churn_data.ipynb` (writing `df_comparison_churn.pickle`, `df_comparison_dummy.pickle`) and `make_uplift_data.ipynb` (writing `df_comparison_uplift.pickle`)
+* Run `make_plots.ipynb`
+
+```
 $ conda create -n uplift_experiment python=3.10
-
-
+$ conda activate uplift_experiment
 $ pip install -r requirements.txt
+```
 
+# References
+
+Based on work described in Radcliffe's papers including "Identifying who can be saved and who will be driven away by retention activity" https://www.stochasticsolutions.com/telcoWhitePaper.html
 
 # TODO
 
 * remove duplication from uplift ml
+* clean up general duplication, run black
--- utility.py
+++ utility.py
@@ -35,3 +35,63 @@ def make_ppl(nbr_rows, base_churn, seed=0):
     # ppl["prob_churn"] = BASE_CHURN  # # a reasonably standard churn rate
     ppl["prob_churn"] = rng.uniform(base_churn - 0.02, base_churn + 0.02, nbr_rows)
     return ppl
+
+def determine_churners(ppl, marketing_prop, seed=0):
+    """People churn based the marketing_prop==[0.0, 1.0] who receive marketing,
+    1.0 means all get it, 0 means none, 0.5 means half"""
+    ppl = ppl.copy()
+    rng = np.random.default_rng(seed=seed)
+    nbr_rows = ppl.shape[0]
+    assert (
+        marketing_prop >= 0 and marketing_prop <= 1.0
+    ), "Must be [0, 1] as a proportion"
+    ppl["gets_mkting"] = rng.binomial(1, marketing_prop, nbr_rows)
+    # people who like marketing and who get marketing have a lower chance of churning
+    mask_mkt_pos = (ppl["mkt_pos"] & ppl["gets_mkting"]) == 1  # trues are 1s (ints)
+    ppl.loc[mask_mkt_pos, "prob_churn"] -= 0.1
+
+    # people who hate marketing and who get marketing have a higher chance of churning
+    mask_mkt_neg = (ppl["mkt_neg"] & ppl["gets_mkting"]) == 1  # trues are 1s (ints)
+    ppl.loc[mask_mkt_neg, "prob_churn"] += 0.1  
+
+    # people who have had a negative experience have a higher chance of churn
+    mask_bad_exp = ppl["bad_exp"] == 1
+    ppl.loc[mask_bad_exp, "prob_churn"] += 0.2
+
+    # people who like the brand experience have a lower chance of churn
+    mask_brand_loyal = ppl["brand_loyal"] == 1
+    ppl.loc[mask_brand_loyal, "prob_churn"] -= 0.2
+
+    ppl["prob_churn"] = ppl["prob_churn"].clip(lower=0, upper=1)
+    ppl["will_churn"] = rng.binomial(1, ppl["prob_churn"], ppl.shape[0])
+    return ppl
+
+def determine_churnersUPLIFT_OLD(ppl, marketing_prop, seed=0):
+    """People churn based the marketing_prop==[0.0, 1.0] who receive marketing,
+    1.0 means all get it, 0 means none, 0.5 means half"""
+    ppl = ppl.copy()
+    rng = np.random.default_rng(seed=seed)
+    nbr_rows = ppl.shape[0]
+    assert (
+        marketing_prop >= 0 and marketing_prop <= 1.0
+    ), "Must be [0, 1] as a proportion"
+    ppl["gets_mkting"] = rng.binomial(1, marketing_prop, nbr_rows)
+    # people who like marketing and who get marketing have a lower chance of churning
+    mask_mkt_pos = (ppl["mkt_pos"] & ppl["gets_mkting"]) == 1  # trues are 1s (ints)
+    ppl.loc[mask_mkt_pos, "prob_churn"] -= 0.1
+
+    # people who hate marketing and who get marketing have a higher chance of churning
+    mask_mkt_neg = (ppl["mkt_neg"] & ppl["gets_mkting"]) == 1  # trues are 1s (ints)
+    ppl.loc[mask_mkt_neg, "prob_churn"] += 0.1
+
+    # people who have had a negative experience have a higher chance of churn
+    mask_bad_exp = ppl["bad_exp"] == 1
+    ppl.loc[mask_bad_exp, "prob_churn"] += 0.2
+
+    # people who like the brand experience have a lower chance of churn
+    mask_brand_loyal = ppl["brand_loyal"] == 1
+    ppl.loc[mask_brand_loyal, "prob_churn"] -= 0.2
+
+    ppl["prob_churn"] = ppl["prob_churn"].clip(lower=0, upper=1)
+    ppl["will_churn"] = rng.binomial(1, ppl["prob_churn"], ppl.shape[0])
+    return ppl
